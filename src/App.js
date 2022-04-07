34c6ea76d270d8c924d190644c0c17f6659966b2
Sanghoon <shpusan001@gmail.com>
2022-04-07 12:31:29 +0900

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import PostListPage from "./pages/PostListPage";
+import WritePage from "./pages/WritePage";
+import { Route, Routes } from "react-router-dom";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={PostListPage} path={["/@:username", "/"]} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
   );
 }
 
---
+++ src/containers/auth/RegisterForm.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AuthForm from "../../components/auth/AuthForm";
+import { changeField, initializeForm } from "../../modules/auth";
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({
+    form: auth.register,
+  }));
+
+  //인풋 변경 이벤트 핸들러
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: "register",
+        key: name,
+        value,
+      }),
+    );
+  };
+
+  //폼 이벤트 등록 핸들러
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    dispatch(initializeForm("register"));
+  }, [dispatch]);
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default RegisterForm;
